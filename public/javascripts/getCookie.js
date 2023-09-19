df1a835494133d663458179e99e3f3180cadadbc
LewisRhine <lewisrhine@gmail.com>
2023-09-19 13:34:05 -0500

    Added test for showing cookies

--- public/javascripts/getCookie.js
+++ public/javascripts/getCookie.js
@@ -1,14 +1,14 @@
 const cookie = document.cookie
 
 document.getElementById('cookieLine').textContent = cookie
-console.log('test log')
-window.addEventListener(
-    "message",
-    (event) => {
-        console.log(event)
-        document.getElementById('cookieLine').textContent = event.data
-    },
-    false,
-);
+// console.log('test log')
+// window.addEventListener(
+//     "message",
+//     (event) => {
+//         console.log(event)
+//         document.getElementById('cookieLine').textContent = event.data
+//     },
+//     false,
+// );
 
-window.postMessage('this is a test')
+// window.postMessage('this is a test')
