a4e6268ec2e2307312547ca752986a082972f3b0
LewisRhine <lewisrhine@gmail.com>
2023-09-19 13:01:11 -0500

    Added test for showing cookies

--- public/javascripts/getCookie.js
+++ public/javascripts/getCookie.js
@@ -5,6 +5,7 @@ document.getElementById('cookieLine').textContent = cookie
 window.addEventListener(
     "message",
     (event) => {
+        console.log(event)
         document.getElementById('cookieLine').textContent = event.data
     },
     false,
