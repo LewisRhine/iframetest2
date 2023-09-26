55c69233622d77ff267bdd42f45685f1dc7c66fa
Daniel Nguyen <dnguyen@higi.com>
2023-09-26 11:28:49 -0500

    testing against web-dev-secure

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+	res.set('Content-Security-Policy', 'frame-ancestors self file://*;')
+	res.render('index', { title: 'Express' });
 });
 
 module.exports = router;
