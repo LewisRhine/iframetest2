fdf35ba841e2523ee4fb5086b0e61d530cd19c55
Daniel Nguyen <dnguyen@higi.com>
2023-10-17 11:59:28 -0500

    testing against web-dev-secure

--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,7 @@ var router = express.Router();
 /* GET home page. */
 router.post('/', function(req, res, next) {
 	res.setHeader('Set-Cookie',
-		"dexcareSession=eyJ1; path=/; domain=.superbuddytime.com; samesite=none; secure; httponly")
+		"dexcareSession=eyJ1; path=/; domain=.superbuddytime.com; Partitioned; samesite=none; secure; httponly")
 	res.setHeader('Content-Security-Policy', 'frame-ancestors self file://*;')
 	res.json({visitRedirectUrl:"https://iframetest-dev.superbuddytime.com"});
 
